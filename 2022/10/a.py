1ab716ca5cc474b0afd4e439bcaaccf1be72e389
Anton Nilsson <antonni@lap5cg91111w6.se.axis.com>
2022-12-10 15:28:06 +0100

    done day 10

---
+++ 2022/10/a.py
@@ -0,0 +1,188 @@
+#!/usr/local/env python
+from pathlib import Path
+
+with (Path(__file__).parent / "input.txt").open() as text:
+
+    lines = [ line.strip() for line in text.read().split("\n") if line]
+
+
+def solve(init_list: list) -> int:
+    X = 1
+    instruct_index = 0
+    do_exec = True
+    signals = []
+    for cycle in range(1, 221):
+        if cycle % 20 == 0 and (cycle - 20) % 40 == 0:
+            sig_str = cycle * X
+            signals.append(sig_str)
+        line = init_list[instruct_index]
+        if line == "noop":
+            instruct_index += 1
+        else:
+            if do_exec:
+                pass
+            else:
+                new_val = int(line[5:])
+                X += new_val
+                instruct_index += 1
+            do_exec = not do_exec
+    return sum(signals)
+
+
+# def solve2(init_list: list) -> int:
+
+
+
+example = [ e.strip() for e in """addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop""".split("\n") if e ]
+
+part1 = solve(example)
+print("Example part 1:", part1)
+assert part1 == 13140
+print("Part 1:", solve(lines))
+
+# 15260 Correct
