f84e4e851a50e81488fd4eb405233861799d6c17
Anton Nilsson <antonni@axis.com>
2023-12-03 07:29:14 +0100

    Day 3

---
+++ 2023/03/a.py
@@ -0,0 +1,21 @@
+import re
+
+def solve(example: list) -> int:
+    numbers_line = []
+    for line in example:
+        numbers_line.append([[m.group(), m.start()] for m in re.finditer(r'\d+', line)])
+
+    ret_val = 0
+    for i, numbers in enumerate(numbers_line):
+        for number in numbers:
+            num_value = number[0]
+            num_index = number[1]
+            num_str = ""
+            for line in range(max(0, i-1), min(i+2, len(example))): # upper bound?
+                _num_str = "".join([example[line][col] for col in range(max(0, num_index-1), min(num_index+len(num_value) + 1, len(example[i])))])
+                num_str += _num_str
+
+            if num_str.replace(".", "") != num_value:
+                ret_val += int(num_value)
+
+    return ret_val
---
+++ 2023/03/b.py
@@ -0,0 +1,32 @@
+import re
+from collections import defaultdict
+import math
+
+def solve(example: list) -> int:
+    numbers_line = []
+    for line in example:
+        numbers_line.append([[m.group(), m.start()] for m in re.finditer(r'\d+', line)])
+
+    gears = defaultdict(lambda: [])
+    ret_val = 0
+    for i, numbers in enumerate(numbers_line):
+        for number in numbers:
+            num_value = number[0]
+            num_index = number[1]
+            num_str = ""
+            gear_coord = None
+            for line in range(max(0, i-1), min(i+2, len(example))):
+                for col in range(max(0, num_index-1), min(num_index+len(num_value) + 1, len(example[i]))):
+                    char = example[line][col]
+                    if char == "*":
+                        gear_coord = (line, col)
+                _num_str = "".join([example[line][col] for col in range(max(0, num_index-1), min(num_index+len(num_value) + 1, len(example[i])))])
+                num_str += _num_str
+            if gear_coord is not None:
+                gears[gear_coord].append(int(num_value))
+
+    for gear_list in gears.values():
+        if len(gear_list) > 1:
+            ret_val += math.prod(gear_list)
+
+    return ret_val
