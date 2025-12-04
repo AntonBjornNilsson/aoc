bf2bef7fb1f29efff4e1a735252e9c03388c261e
AntonBjornNilsson <antonni@axis.com>
2025-12-04 12:29:00 +0100

    day 4

---
+++ 2025/04/b.py
@@ -0,0 +1,50 @@
+from typing import List
+from copy import deepcopy
+
+
+def pp(grid: list) -> None:
+    for y in grid:
+        for x in y:
+            print(x, end="")
+        print()
+
+
+def selection_filter(grid: list, positions: list) -> int:
+    ret = 0
+    for pos in positions:
+        y, x = pos
+        if grid[y][x] == "@":
+            ret += 1
+    if ret < 4:
+        return 1
+    return 0
+
+
+def solve(example: List[str]) -> int:
+    ret_val = 0
+    last_iter = -1
+    grid = []
+    for i, line in enumerate(example):
+        grid.append([x for x in line])
+        
+    while last_iter != ret_val:
+        last_iter = ret_val
+        for iy, y in enumerate(grid):
+            for ix, x in enumerate(y):
+                if grid[iy][ix] != "@":
+                    continue
+                positions = [
+                    (delta_y, delta_x)
+                    for delta_x in range(ix - 1, ix + 2)
+                    for delta_y in range(iy - 1, iy + 2)
+                    if (delta_x, delta_y) != (ix, iy)
+                    and (delta_x >= 0 and delta_x < len(y))
+                    and (delta_y >= 0 and delta_y < len(grid))
+                ]
+
+                ret_val += selection_filter(grid, positions)
+                if selection_filter(grid, positions) != 0:
+                    grid[iy][ix] = "."
+
+        # pp(fancy_grid)
+    return ret_val
---
+++ 2025/04/run.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from parsers import parse_raw
+from validation import validate
+from .a import solve
+from .b import solve as solve2
+
+parse_function = parse_raw
+
+lines = parse_function(__file__, "input")
+ex_lines_a = parse_function(__file__, "example_a")
+# example b needs to be created manually if it exists
+ex_lines_b = (
+    parse_function(__file__, "example_b")
+    if (Path(__file__).parent / "example_b.txt").is_file()
+    else ex_lines_a
+)
+
+example_a = solve(ex_lines_a)
+# validate(__file__, example1=example_a)
+assert (
+    example_a == 13
+), f"{example_a} is not equal to "  # comment out when aocd is not working
+solution_a = solve(lines)
+validate(__file__, answer1=solution_a)
+example_b = solve2(ex_lines_b)
+# validate(__file__, example2=example_b)
+assert example_b == 43, f"{example_b} is not equal to 43" # comment out when aocd is not working
+solution_b = solve2(lines)
+validate(__file__, answer2=solution_b)
