8710ef35ea1ea6b3684bbe414d6540804fb47a94
AntonBjornNilsson <antonni@axis.com>
2025-12-06 08:09:44 +0100

    day 6

---
+++ 2025/06/a.py
@@ -0,0 +1,18 @@
+import re
+from typing import List
+from regex import find_all_int
+import operator
+from functools import reduce
+
+def solve(example: List[str]) -> int:
+    ret_val = 0
+    problems_grid = []
+    for i, line in enumerate(example[:-1]):
+        problems_grid.append([ int(x) for x in find_all_int(line)])
+    signs = [operator.add if x == '+' else operator.mul for x in re.findall(r"\S", example[-1])]
+    
+    for i, op in enumerate(signs):
+        curr = [x[i] for x in problems_grid ]
+        ret_val += reduce(op, curr)
+
+    return ret_val
---
+++ 2025/06/b.py
@@ -0,0 +1,35 @@
+import re
+from typing import List
+from regex import find_all_int
+import operator
+from functools import reduce
+
+
+def solve(example: List[str]) -> int:
+    ret_val = 0
+    op = operator.add
+    saved = []
+    example = [x + " " for x in example]
+    for yi in range(max([len(x) for x in example])-1):
+        cols = []
+        o = example[len(example)-1][yi]
+        if o == "*":
+            op = operator.mul
+        if o == '+':
+            op =operator.add
+
+        for xi in range(len(example)-1):
+            c = example[xi][yi]
+            if c != ' ':
+                cols.append(c)
+        if cols == [] or '\n' in cols:
+            value = reduce(op, saved)
+            ret_val += value
+            saved = []
+        else:
+            saved.append(int("".join(cols)))
+    
+
+    return ret_val
+# remove line.strip() from parser to make it work
+# to sleepy to make it nice
